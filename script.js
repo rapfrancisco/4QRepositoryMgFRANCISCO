b7cf2a1d1485a9e5944e58f93683a985735da373
rapfrancisco <b2029rapfrancisco@pshs.edu.ph>
2026-03-10 00:29:44 +0800

    csc

--- script.js
+++ script.js
@@ -41,9 +41,13 @@ function displayMovies() {
     let lelist = document.getElementById("listofmovies");
     lelist.innerHTML = "";
 
+    movies.forEach(function(m)) { //foreach star
+        let stars = "";
+        for (let i = 1; i <= m.rating; i++) 
+            stars += "★"; //ungreyed stars
+        }
+
     movies.forEach(function(m) {
         lelist.innerHTML += `<p>${m.title} (${m.year}) - ${m.genre} - Rating: ${m.rating}</p>`; //the display of what got stored
     });
 }
-
-
